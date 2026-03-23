ffa1d959b27fde0da33280bae198d19e19213c56
jumana2025 <jumana.edv@gmail.com>
2026-03-23 17:19:57 +0530

    Reset migrations and database

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'writings',
+    'events',
 ]
 
 MIDDLEWARE = [
@@ -121,8 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-CORS_ALLOW_ALL_ORIGINS = [
-    "https://bookmaas.onrender.com",
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    "https://bookma-front.vercel.app",
+    "http://localhost:5173", 
 ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
